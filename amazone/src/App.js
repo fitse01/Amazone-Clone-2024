929502ee5d6191da7c9a5d4b05fe0344280d65eb
fitse01 <135918799+fitse01@users.noreply.github.com>
2024-01-31 13:18:59 +0300

    amazone setup

---
+++ amazone/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Amazone clone
+    </div>
+  );
+}
+
+export default App;
+
+
+// npm i react react-dom @trendyol-js/react-carousel --save
+// import { SlLocationPin } from "react-icons/sl";
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Carousel } from '@trendyol-js/react-carousel';
+// import { Item } from './yourItem';
+
+// ReactDOM.render(
+// 	<Carousel>
+// 		<Item />
+// 		<Item />
+// 		<Item />
+// 		<Item />
+// 	</Carousel>,
+// 	document.getElementById('root'),
+// );
