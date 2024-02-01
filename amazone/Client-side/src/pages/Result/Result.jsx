2bb76e56047295669cf858aacd0578035a6d19e4
fitse01 <135918799+fitse01@users.noreply.github.com>
2024-02-01 07:25:04 +0300

    Routing setUp finished

--- amazone/Client-side/src/pages/Result/Result.jsx
+++ amazone/Client-side/src/pages/Result/Result.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import classes from "./Result.module.css"
 import LayOut from '../../Components/LayOut/LayOut'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { productUrl } from '../../Api/endPoints'
 
 function Result() {
+  const {categoryName} = useParams()
+  // console.log(categoryName)
+  // axios.get(`${productUrl}/products/category/${categoryName}`)
+  //   .then((res)=>{
+  //     console.log(res)
+
+  //   })
   return (
     <LayOut>
       <div>Result</div>
